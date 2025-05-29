41fdd810a1e6504863c15260a436e8db81310a9a
kekley <jesusmacias8568@gmail.com>
2025-05-28 21:44:55 -0700

    lil change to the remaps so they work on windows

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -2,13 +2,13 @@ vim.g.mapleader = " "
 vim.g.localleader = "\\"
 
 --open remap file
-vim.keymap.set("n", "<Leader>vr", ":e ~/.config/nvim/lua/config/remaps.lua <cr>")
+vim.keymap.set("n", "<Leader>vr", ":e " .. vim.fn.stdpath("config") .. "/lua/config/remaps.lua <cr>")
 
 --open opts file
-vim.keymap.set("n", "<Leader>vo", ":e ~/.config/nvim/lua/config/opts.lua <cr>")
+vim.keymap.set("n", "<Leader>vo", ":e " .. vim.fn.stdpath("config") .. "/lua/config/opts.lua <cr>")
 
 --resource init.lua
-vim.keymap.set("n", "<Leader><CR>", ":so ~/.config/nvim/init.lua <CR>")
+vim.keymap.set("n", "<Leader><CR>", ":so " .. vim.fn.stdpath("config") .. "/init.lua <CR>")
 
 --explorer binds
 vim.keymap.set("n", "<Leader>e", ":NvimTreeFindFile<CR>")
@@ -17,4 +17,4 @@ vim.keymap.set("n", "<Leader>e", ":NvimTreeFindFile<CR>")
 vim.keymap.set("n", "<Leader>cd", ':cd <C-R>=expand("%:p:h")<CR><CR>')
 
 --open file explorer at plugins folder
-vim.keymap.set("n", "<Leader>vp", ":e ~/.config/nvim/lua/plugins/ <CR>")
+vim.keymap.set("n", "<Leader>vp", ":e " .. vim.fn.stdpath("config") .. "/lua/plugins/ <CR>")
