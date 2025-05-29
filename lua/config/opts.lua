608c94532f505035a7ed72e283ebf1d9abac5fc6
kekley <111942843+kekley@users.noreply.github.com>
2025-05-28 23:57:41 -0700

    help

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -3,31 +3,48 @@ vim.opt.relativenumber = true
 vim.opt.signcolumn = "number"
 vim.wo.foldmethod = 'expr'
 vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
-vim.opt.expandtab=true
-vim.opt.tabstop=4
-vim.opt.softtabstop=4
-vim.opt.shiftwidth=4
-vim.opt.smartindent=true
-vim.opt.scrolloff=8
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.scrolloff = 8
+
+
+
+local sign = function(opts)
+    vim.fn.sign_define(opts.name, {
+        texthl = opts.name,
+        text = opts.text,
+        numhl = ''
+    })
+end
+
+sign({ name = 'DiagnosticSignError', text = '' })
+sign({ name = 'DiagnosticSignWarn', text = '' })
+sign({ name = 'DiagnosticSignHint', text = '' })
+sign({ name = 'DiagnosticSignInfo', text = '' })
 
 vim.diagnostic.config({
-	virtual_text=true,
-	signs=true,
-	update_in_insert=true,
-	underline=true,
-	severity_sort=false,
-	float = {
-		border = 'rounded',
-		source = 'always',
-		header = '',
-		prefix = '',
-	},
+    virtual_text = false,
+    signs = true,
+    update_in_insert = true,
+    underline = true,
+    severity_sort = false,
+    float = {
+        border = 'rounded',
+        source = 'always',
+        header = '',
+        prefix = '',
+    },
 })
 
-
+vim.cmd([[
+set signcolumn=yes
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
 vim.o.updatetime = 750
 
-vim.cmd([[
-	set signcolumn=yes
-	autocmd CursorHold * lua vim.diagnostic.open_float(nil,{focusable=false})
-]])
+vim.opt.completeopt = { 'menuone', 'noselect', 'noinsert' }
+vim.opt.shortmess = vim.opt.shortmess + { c = true }
+vim.api.nvim_set_option('updatetime', 300)
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,32 +1,32 @@
 return{
-    "mason-org/mason.nvim",
-    opts = {
-        ensure_installed = {
-      "asmfmt",
-      "autopep8",
-      "beautysh",
-      "clang-format",
-      "cmakelang",
-      "codelldb",
-      "codespell",
-      "cpplint",
-      "csharpier",
-      "htmlbeautifier",
-      "htmlhint",
-      "luacheck",
-      "stylua",
-      "textlint",
-      "yamlfix",
-      "gh",
-      "gitui",
-      "glow",
-    },
-        ui = {
-            icons = {
-                package_installed = "✓",
-                package_pending = "➜",
-                package_uninstalled = "✗"
-            }
-        }
-    }
-}
+			"williamboman/mason.nvim",
+			opts = {
+				ensure_installed = {
+					"asmfmt",
+					"autopep8",
+					"beautysh",
+					"clang-format",
+					"cmakelang",
+					"codelldb",
+					"codespell",
+					"cpplint",
+					"csharpier",
+					"htmlbeautifier",
+					"htmlhint",
+					"luacheck",
+					"stylua",
+					"textlint",
+					"yamlfix",
+					"gh",
+					"gitui",
+					"glow",
+				},
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			},
+		}
