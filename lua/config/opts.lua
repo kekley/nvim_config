bc98dd2e039c2f04bfca98250ba0f7978f4baa77
kekley <jesusmacias8568@gmail.com>
2025-08-18 13:04:39 -0700

    better inline diagnostics

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -12,23 +12,19 @@ vim.opt.scrolloff = 8
 vim.opt.wrap = false
 
 vim.diagnostic.config({
-	virtual_text = true,
+	virtual_text = false,
 	signs = true,
-	update_in_insert = true,
-	underline = true,
-	severity_sort = false,
+	update_in_insert = false,
+	underline = false,
+	severity_sort = true,
 	float = {
 		border = "rounded",
-		source = "always",
+		source = "if_many",
 		header = "",
 		prefix = "",
 	},
 })
 
-vim.cmd([[
-set signcolumn=yes
-autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
-]])
 vim.o.updatetime = 750
 vim.lsp.inlay_hint.enable(true)
 vim.opt.completeopt = { "menuone", "noselect", "noinsert" }
