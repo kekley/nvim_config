05822a5f7408d8531888d1d149c1ff6cacf2ebf4
kekley <jesusmacias8568@gmail.com>
2025-10-18 13:59:34 -0700

    enable gdscript, c#, python lsps

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -4,10 +4,10 @@ return {
 		{
 			"neovim/nvim-lspconfig",
 			config = function()
-				vim.lsp.config("csharp_ls", {})
-				vim.lsp.enable("csharp_ls")
 				vim.lsp.config("gdscript", {})
 				vim.lsp.enable("gdscript")
+				vim.lsp.config("csharp_ls", {})
+				vim.lsp.enable("csharp_ls")
 				vim.lsp.config("cssls", {})
 				vim.lsp.enable("cssls")
 				vim.lsp.config("html", {})
@@ -16,8 +16,7 @@ return {
 				vim.lsp.enable("htmx")
 				vim.lsp.config("typescript-language-server", {})
 				vim.lsp.enable("typescript-language-server")
-				vim.lsp.config("pyright", {})
-				vim.lsp.enable("pyright")
+				vim.lsp.enable("basedpyright")
 				vim.lsp.config("lua_ls", {})
 				vim.lsp.enable("lua_ls")
 				vim.lsp.config("clangd", { init_options = { compilationDatabasePath = "./" } })
